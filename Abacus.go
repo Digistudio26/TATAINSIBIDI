63b532a813ad75c33a2063c82eab468c44c21eb0
Ndi Tata Charles <k-ntatac@c3r4p3.hive.fi>
2026-01-20 21:42:28 +0200

    Abacus.go

--- Abacus.go
+++ Abacus.go
@@ -1,10 +1,8 @@
-// You can edit this code!
-// Click here and start typing.
 package main
+
+func Abacus(a, b float64) float64 {
+	return a / b
+}
 
 func main() {
 	println(Abacus(15, 10))
-}
-func Abacus(a int, b int) int {
-	return a / b
-}
