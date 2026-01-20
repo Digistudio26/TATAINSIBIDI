72f168d47bfc1cad94d00218b3a43d8c6229c234
Ndi Tata Charles <k-ntatac@c3r4p3.hive.fi>
2026-01-20 21:17:29 +0200

    Abacus.sh

---
+++ Abacus.sh
@@ -0,0 +1,15 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+func main() {
+	println(Abacus(15, 10))
+}
+func Abacus(a int, b int) int {
+	return a / b
+}package sprint
+
+func Abacus(a int, b int) int {
+
+}
+return(a, b)
