40ed2791fbaa917b97df5771114b65625a69c540
Ndi Tata Charles <k-ntatac@c2r2p7.hive.fi>
2026-01-22 20:18:11 +0200

    accumulate.go

--- accumulate.go
+++ accumulate.go
@@ -1,9 +1,5 @@
-package main
+package sprint
 
-import "fmt"
-
-// Accumulate returns the sum of all numbers from 0 to n if n is positive.
-// If n is negative, it returns 0.
 func Accumulate(n int) int {
 	if n < 0 {
 		return 0
@@ -17,9 +13,5 @@ func Accumulate(n int) int {
 	return sum
 }
 
-func main() {
-	fmt.Println(Accumulate(4))  // Output: 10
-	fmt.Println(Accumulate(0))  // Output: 0
-	fmt.Println(Accumulate(-5)) // Output: 0
-}     
+
 
