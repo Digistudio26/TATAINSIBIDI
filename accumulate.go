d9efd4ccd48320fcad37544bf535bff040ad0469
Ndi Tata Charles <k-ntatac@c2r2p7.hive.fi>
2026-01-22 19:36:54 +0200

    accumulation

---
+++ accumulate.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func Accumulate(n int) int {
+	if n < 0 {
+		return 0
+	}
+
+	sum := 0
+	for i := 0; i <= n; i++ {
+		sum += i
+	}
+
+	return sum
+}
+
+func main() {
+	fmt.Println(Accumulate(4))  // Output: 10
+	fmt.Println(Accumulate(0))  // Output: 0
+	fmt.Println(Accumulate(-5)) // Output: 0
+}
