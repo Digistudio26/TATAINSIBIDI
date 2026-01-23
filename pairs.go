28ad0b179d4f5940f2550c220ed060b3ebe88503
Ndi Tata Charles <k-ntatac@c2r2p7.hive.fi>
2026-01-23 21:30:40 +0200

    pairs

--- pairs.go
+++ pairs.go
@@ -1,19 +1,18 @@
 package main
 
-import "fmt"
-
-
-
 func Pairs() string {
 	result := ""
+
 	for i := 0; i <= 98; i++ {
 		for j := i + 1; j <= 99; j++ {
-			pair := fmt.Sprintf("%02d %02d", i, j)
 			if result != "" {
-				result += ","
+				result += ", "
 			}
-			result += pair
+			result += string('0'+i/10) + string('0'+i%10) +
+				" " +
+				string('0'+j/10) + string('0'+j%10)
 		}
 	}
+
 	return result
 }
