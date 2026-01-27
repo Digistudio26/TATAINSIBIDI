d2a88834715083332dabc71701d0fc2e8a6ad8dd
Ndi Tata Charles <k-ntatac@c2r2p7.hive.fi>
2026-01-27 18:05:41 +0200

    Remove

---
+++ removeelementsinrange.go
@@ -0,0 +1,28 @@
+package sprint
+
+func main() {
+	fmt.Println(RemoveElementsInRange([]float64{10., .8, -.4, 20., 7.7, 3.}, 4, 1))
+}
+
+func RemoveElementsInRange(arr []float64, from, to int) []float64 {
+	// Ensure correct order
+	if from > to {
+		from, to = to, from
+	}
+
+	
+	if from < 0 {
+		from = 0
+	}
+	if to > len(arr) {
+		to = len(arr)
+	}
+
+	// If nothing to remove, return original array
+	if from >= to {
+		return arr
+	}
+
+	// Remove elements in range [from, to)
+	return append(arr[:from], arr[to:]...)
+}
