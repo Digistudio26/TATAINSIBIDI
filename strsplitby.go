2744c523bb490f39fc1280cfa9f23ac6cbde7724
Ndi Tata Charles <k-ntatac@c2r2p7.hive.fi>
2026-01-30 19:16:43 +0200

    Strsplit

---
+++ strsplitby.go
@@ -0,0 +1,25 @@
+package main
+
+
+func strSplitBy(s, sep string) []string {
+	if sep == "" {
+		return []string{s}
+	}
+
+	var result []string
+	start := 0
+	i := 0
+
+	for i <= len(s)-len(sep) {
+		if s[i:i+len(sep)] == sep {
+			result = append(result, s[start:i])
+			i += len(sep)
+			start = i
+		} else {
+			i++
+		}
+	}
+
+	result = append(result, s[start:])
+	return result
+}
