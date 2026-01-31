8abdf2ec3879a1308ea2c6fc0fe2a8f5759c3ecd
Ndi Tata Charles <k-ntatac@c2r2p7.hive.fi>
2026-01-31 12:23:06 +0200

    newStruct

---
+++ newstruct.go
@@ -0,0 +1,15 @@
+package main
+
+
+type poin struct {
+	X float32.
+	Y float32
+	Text string
+}
+	func makepoint (x, y float32,textstring) point{
+		return point {
+			X: x,
+			Y: y,
+			Text: text
+		}
+	}
