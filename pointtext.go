381a4c48ef0c89155c32ede8cc09afc5bb2f5f3c
Ndi Tata Charles <k-ntatac@c2r2p7.hive.fi>
2026-02-01 12:08:42 +0200

    commit pointtext

---
+++ pointtext.go
@@ -0,0 +1,15 @@
+package main
+
+type Point struct {
+	X    int
+	Y    int
+	Text string
+}
+
+func PointText(p Point) Point {
+	return Point{
+		X:    p.X,
+		Y:    p.Y,
+		Text: fmt.Sprintf("Text at (%d, %d)", p.X, p.Y),
+	}
+}
