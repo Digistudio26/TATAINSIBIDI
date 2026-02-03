3c9961ca98dbe4c0954514de3a13dde6643c78df
Ndi Tata Charles <k-ntatac@c2r2p7.hive.fi>
2026-02-03 11:49:10 +0200

    Factorials

--- factorialrecursive.go
+++ factorialrecursive.go
@@ -1,6 +1,16 @@
-package main
+func FactorialRecursive(n int) int {
+	if n < 0 {
+		return 0 // factorial not defined for negatives
+	}
 
+	if n == 0 {
+		return 1 // base case
+	}
 
+	return n * FactorialRecursive(n-1) // recursive step
+}
+
+// Iterative factorial
 func FactorialIterative(n int) int {
 	if n < 0 {
 		return 0 // factorial not defined for negatives
