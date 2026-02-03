af8e9aa8f1b40e09e857e81b8275bbcc84406edd
Ndi Tata Charles <k-ntatac@c2r2p7.hive.fi>
2026-02-03 11:39:57 +0200

    factors

--- factorialrecursive.go
+++ factorialrecursive.go
@@ -1,13 +1,13 @@
 package main
 
+
 func FactorialIterative(n int) int {
 	if n < 0 {
-		return 0
+		return 0 // factorial not defined for negatives
 	}
 
 	result := 1
-
-	for i := n; i >= 1; i-- {
+	for i := 1; i <= n; i++ { // multiply 1 × 2 × ... × n
 		result *= i
 	}
 
